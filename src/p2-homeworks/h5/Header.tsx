fad2a128c00f1d8e6fc2421a1521954f74fb4c40
Aleksandr <deadreebook@gmail.com>
2022-01-07 19:12:18 +0700

    added Routing

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {PATH} from "./Rout";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <div>
+                <NavLink to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+            </div>
+            <div>
+                <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+            </div>
+            <div>
+                <NavLink to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>
+            </div>
 
         </div>
     )
